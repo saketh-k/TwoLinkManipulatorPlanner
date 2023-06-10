d3f22fa16944b2670426216950dc7864a53410e3
Elizabeth Marie Chiu <chiuem@uci.edu>
2023-06-09 19:42:10 -0700

    saketh give me more saketh points

--- Project1_light.m
+++ Project1_light.m
@@ -1,5 +1,4 @@
 %clc;clear;
-
 clear all
 
 label_location=[1.25 sqrt(3)/4;
@@ -7,8 +6,6 @@ label_location=[1.25 sqrt(3)/4;
                 0.6    0.6
                 -sqrt(2)/4  -1-sqrt(2)/4
                 -0.5 -0.5]
-
-
 
 % Generate the collision-free configuration space
 obs = [0.5 0.25 0.25 0.25; 
@@ -44,12 +41,7 @@ fig_PRM=figure();
 fig_samplesChil = fig_samples.Children; 
 copyobj(fig_samplesChil, fig_PRM);
 
-
-
 % [adjacency, weights] = generateGraph(C, th1, th2);
-
-
-
 
 hold on
 edge_length=-1*ones(n_samples+2,n_samples+2);%we initialize this matrix -1; at the end of process if an element is -1 it means that the corresponding elements are not connected.
@@ -90,14 +82,21 @@ start_point = [0.1, 0.125];
 goal_point = [5.2, 1];
 goal2_point = [2, 5];
 
-thetas1 = findPath(Samples, n_samples, Adj_table, r, edge_length, start_point, goal_point);
-all_thetas = thetas1;
-
-thetas2 = findPath(Samples, n_samples, Adj_table, r, edge_length, goal_point, start_point);
-
-
-
-animateArm(len1, len2, [thetas1', thetas2']', obs, label_location);
+% thetas1 = findPath(Samples, n_samples, Adj_table, r, edge_length, start_point, goal_point);
+% all_thetas = thetas1;
+% 
+% thetas2 = findPath(Samples, n_samples, Adj_table, r, edge_length, goal_point, start_point);
+tic
+all_thetas = [];
+first_location = start_point;
+for i=0:(length(label_location)-1)
+    thetas1 = findPath(Samples, n_samples, Adj_table, r, edge_length, first_location, label_location(i+1,:));
+    all_thetas = [all_thetas' thetas1']';
+    first_location = label_location(i+1,:);
+end
+thetas4 = findPath(Samples, n_samples, Adj_table, r, edge_length, label_location(end,:),goal_point);
+toc
+animateArm(len1, len2, [all_thetas], obs, label_location);
 
 
 
